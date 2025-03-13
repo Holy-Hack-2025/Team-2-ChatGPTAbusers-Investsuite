42fb041bd351ca7d0c88593e5b29e14b84cf54eb
J-VD-M <r0950937@ucll.be>
2025-03-13 23:09:45 +0100

    changes

--- src/frontend/components/Header.tsx
+++ src/frontend/components/Header.tsx
@@ -19,15 +19,21 @@ const Header: React.FC = () => {
     };
 
     return (
-        <header className="p-4 border-bottom bg-gradient-to-br from-gray-900 to-gray-600 flex items-center justify-between">
+        <header className="p-4 border-bottom bg-gradient-to-br from-purple-900 to-purple-600 flex items-center justify-between">
             <div className="flex items-center gap-2">
                 <h1 className="text-white text-4xl">Frontend</h1>
             </div>
             <nav className="flex items-center">
+                <Link
+                    href="/"
+                    className="px-4 py-2 text-white text-xl text-white hover:bg-purple-600 rounded-lg"
+                >
+                    Home
+                </Link>
                 {!loggedInUser && (
                     <Link
                         href="/auth"
-                        className="px-4 py-2 text-white text-xl hover:bg-gray-600 rounded-lg"
+                        className="px-4 py-2 text-white text-xl hover:bg-purple-600 rounded-lg"
                     >
                         Login
                     </Link>
@@ -36,20 +42,14 @@ const Header: React.FC = () => {
                     <>
                         <Link
                             href="/"
-                            className="px-4 pt-1 pb-2 text-white text-xl text-white hover:bg-gray-600 rounded-lg"
-                        >
-                            Home
-                        </Link>                        
-                        <Link
-                            href="/"
                             onClick={handleLogout}
-                            className="px-4 pt-1 pb-2 text-white text-xl text-white hover:bg-gray-600 rounded-lg"
+                            className="px-4 py-2 text-white text-xl text-white hover:bg-purple-600 rounded-lg"
                         >
                             Logout
                         </Link>
                         <Link
                             href="/account"
-                            className="px-4 pt-1 pb-2 text-white text-xl text-white hover:bg-gray-600 border-l"
+                            className="px-4 py-2 text-white text-xl text-white hover:bg-purple-600 border-l"
                         >
                             {loggedInUser.username}
                         </Link>{' '}
