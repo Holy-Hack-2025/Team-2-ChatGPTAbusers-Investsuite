e8b5384e51996a63ce7e7e3e50e62aacfae191bb
Lander Dirix <lander.dirix@student.ucll.be>
2025-03-14 11:52:12 +0100

    peepee

--- src/frontend/pages/quiz/index.tsx
+++ src/frontend/pages/quiz/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Header from '@components/Header';
 import quizService from '@services/quizService';
+import { useState } from 'react';
+import QuestionOverview from '@components/Quiz/QuestionOverview';
 
 const Quiz: React.FC = () => {
+    const [mode, setMode] = useState<string>("start");
+    const [question, setQuestion] = useState<string>("");
 
     const getQuestion = async () => {
         const response = await quizService.getQuestion();
@@ -10,8 +14,11 @@ const Quiz: React.FC = () => {
         if (!response.ok) {
             console.error('Failed to get question');
         } else {
-            console.log(getQuestion);
-            getQuestion();
+            const result = await response.json();
+            console.log(result);
+
+            setQuestion(result);
+            setMode('question');
         };
     }
 
@@ -25,10 +32,16 @@ const Quiz: React.FC = () => {
             </Head>
             <Header></Header>
             <main className="h-screen bg-stone-100 content-center">
-                <div className="flex flex flex-col items-center">
-                    <h1 className="mt-2 text-7xl text-gray-800">Test your knowledge! ignore this bitch </h1>
+                {mode == 'start' && (
+                    <div className="flex flex-col items-center">
+                    <h1 className="mt-2 text-7xl text-gray-800">Test your knowledge!</h1>
                     <button className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={getQuestion}>get question</button>
                 </div>
+                )}
+                {mode == 'question' && (
+                    <QuestionOverview question={question} />
+                )}
+                
             </main>
         </>
     );
