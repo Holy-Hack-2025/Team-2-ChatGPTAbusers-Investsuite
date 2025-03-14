4d7ef4b1f18ef7e53ac85bdfb9f7ff5f59363820
Christophe-Gervais <christophe.gervais@student.ucll.be>
2025-03-14 13:03:25 +0100

    trust 2

--- src/frontend/components/Quiz/QuestionOverview.tsx
+++ src/frontend/components/Quiz/QuestionOverview.tsx
@@ -1,20 +1,48 @@
+import LineGraph from '@components/graphs/LineGraph';
+import PieGraph from '@components/graphs/PieGraph';
+
 type Props = {
     question: any;
     handleAnswer: (answer: string) => void;
 };
 
 const QuestionOverview: React.FC<Props> = ({ question, handleAnswer }: Props) => {
+    if (!question || !question.options) {
+        return <p className="text-red-500">No data available.</p>;
+    }
+
+    console.log("Rendering QuestionOverview with:", question);
+
     return (
         <div className="flex flex-col w-full">
             <h3 className="text-black text-center">What is the graph of '{question.token}'</h3>
             <div className="text-black grid grid-cols-2 gap-3 p-3">
-                <button className="w-full bg-blue-300 rounded p-8 cursor-pointer" onClick={x => handleAnswer(question.options[0].token)}>{question.options[0].token}</button>
-                <button className="w-full bg-red-300 rounded p-8 cursor-pointer" onClick={x => handleAnswer(question.options[1].token)}>{question.options[1].token}</button>
-                <button className="w-full bg-yellow-300 rounded p-8 cursor-pointer" onClick={x => handleAnswer(question.options[2].token)}>{question.options[2].token}</button>
-                <button className="w-full bg-green-300 rounded p-8 cursor-pointer" onClick={x => handleAnswer(question.options[3].token)}>{question.options[3].token}</button>
+                {question.options.map((option: any, index: number) => (
+                    <button
+                        key={index}
+                        className="w-full bg-gray-200 rounded p-4 cursor-pointer flex flex-col items-center gap-2"
+                        onClick={() => handleAnswer(option.token)}
+                    >
+                        <span className="text-lg font-semibold">{option.token}</span>
+
+                        {option.type === 'line' && option.data ? (
+                            <LineGraph 
+                                lineGraphOptions={{
+                                    title: option.token,
+                                    yTitle: 'Value',
+                                    xTitle: 'Time',
+                                    start: 2015,
+                                    series: [{data:option.data, name: option.title}]
+                                }}
+                            />
+                        ) : (
+                            <p className="text-red-500">Invalid graph data</p>
+                        )}
+                    </button>
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default QuestionOverview;
